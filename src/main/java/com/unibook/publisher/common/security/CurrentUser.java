a67baba4a401c2982fe47ae5cc76dd4632999865
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-09-13 18:57:52 +0300

    Fix: delete deprecated data

--- src/main/java/com/unibook/publisher/common/security/CurrentUser.java
+++
@@ -1,8 +0,0 @@
-package com.unibook.publisher.common.security;
-
-import com.unibook.publisher.common.enums.UserRole;
-
-import java.util.UUID;
-
-public record CurrentUser(UUID id, UserRole role) {
-}
